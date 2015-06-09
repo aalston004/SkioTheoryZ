ee7ea6187d7725a88679346a5a847b8d949bef15
aalston004 <aalston004@gmail.com>
2015-06-09 18:51:42 -0400

    testing _s

--- wp-content/themes/mAAd Villain/header.php
+++ wp-content/themes/mAAd Villain/header.php
@@ -20,35 +20,18 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-     <header>
-			<nav class="navbar col-md-9 navbar-right" role="navigation">
-                <div class="container-fluid"> <!---b-->
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header"><!--c-->
-                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>                                              
-                    </div><!--c-->
-                    
-                      <?php
-                            wp_nav_menu( array(
-                                'menu'              => 'primary',
-                                'theme_location'    => 'primary',
-                                'depth'             => 2,
-                                'container'         => 'div',
-                                'container_class'   => 'collapse navbar-collapse',
-                                'container_id'      => 'bs-example-navbar-collapse-1',
-                                'menu_class'        => 'nav navbar-nav',
-                                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                                'walker'            => new wp_bootstrap_navwalker())
-                            );
-                        ?>
+     <header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		</div>
 
-                    </div><!--b-->
-                </nav>
-	</header>
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle"><?php _e( 'Primary Menu', 'ta3t-core' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+
 
 	<div id="content" class="site-content">
